ef33700f550ffbeff10cc726b3ea7ed8b2ba78c4
nictheriscer <nicolas.riscalas@stu.ocsb.ca>
2022-02-24 01:33:13 +0000

    created basic math and area of rectangle

---
+++ areaPerRect.cpp
@@ -0,0 +1,18 @@
+// Copyright (c) 2022 Mr. Riscalas All rights reserved
+//
+// Created By: Nicolas Riscalas
+// Created On February 17 2022
+// Area of the rectangle
+
+
+#include <iostream>
+using std::cout;
+
+
+int area = 7*8;
+int perimeter = 2*(7+8);
+int main() {
+    cout << "If a rectangle has the dimensions:\n7cm x 8cm\n";
+    cout << "The area is: " <<area;
+    cout << "\nThe perimeter is: " <<perimeter;
+}
---
+++ basicMath.cpp
@@ -0,0 +1,20 @@
+// Copyright (c) 2022 Mr. Riscalas All rights reserved
+//
+// Created By: Nicolas Riscalas
+// Created On February 17 2022
+// Hello world with proper style
+
+
+#include <iostream>
+#include <cmath>
+using std::cout;
+
+
+int main() {
+    cout << "8 + 3 :" << 8 + 3;
+    cout << "\n8 - 3 :" << 8 - 3;
+    cout << "\n8 / 3 :" << 8 / 3;
+    cout << "\n8 * 3 :" << 8 * 3;
+    cout << "\n8 ** 3 :" << std::pow(8, 3);
+    cout << "\n8 ** 1/3 :" << pow(8, 1/3);
+}
